a37f33e24cdaea97ae9e72f230bec48a4cecb3f7
Nivetha1492 <nivethanive93@gmail.com>
2024-07-29 13:56:26 +0530

    Add files via upload

---
+++ week2day2/week2day4/Browser.java
@@ -0,0 +1,25 @@
+package week2day4;
+
+public class Browser {
+	
+	String browserName = "Chrome";
+	float browserVersion = 128.0f;
+	
+	public void openURL() {
+		
+		System.out.println("url is opened");
+		
+		}
+	public void closeBrowser() {
+		
+		System.out.println("browser is closed");
+	}
+	
+	 public void navigateBack() {
+		 
+		 System.out.println("navigate back");
+	 }
+
+	}
+
+
